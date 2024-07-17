ba46033fe9bd0d6ee209c55fa0b141a7bfdfaf15
Robbert <robbert.mediahuis@gmail.com>
2024-07-17 15:58:53 +0200

    git actions 6

--- api_integration_testing/module_c/setup/cognito_token.py
+++ api_integration_testing/module_c/setup/cognito_token.py
@@ -14,7 +14,11 @@ async def get_cognito_token():
 async def write_cognito_token():
     token = await generate_cognito_token()
     async with aiofiles.open(token_file, mode="w") as f:
-        await f.write(token)
+        if token is not None:
+            await f.write(token)
+        else:
+            # Handle the case where token is None, e.g., log an error or raise an exception
+            print("Token is None. Cannot write to file.")
 
 async def unlink_cognito_token():
     os.remove(token_file) # Delete the file
